493077af7d81a1e3577858baa81ee06a07c5b86a
Alexandru Pall <alexandru@crisser.ro>
2017-01-09 11:40:36 +0200

    Completed getting firebase data

--- SocialFirebase/FeedVC.swift
+++ SocialFirebase/FeedVC.swift
@@ -20,6 +20,11 @@ class FeedVC: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
+        DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
+            
+           print(snapshot.value)
+        })
+        
     }
 
    
